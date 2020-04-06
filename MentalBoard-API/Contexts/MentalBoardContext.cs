36e9a2887e8879cabf02bff65afc137813134fe4
LauryV <laury.vaquedano@yahoo.com>
2020-04-06 00:29:24 -0600

    Database connection

---
+++ MentalBoard-API/Contexts/MentalBoardContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MentalBoard_API.Contexts
+{
+    public class MentalBoardContext : DbContext
+    {
+        public MentalBoardContext(DbContextOptions<MentalBoardContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
