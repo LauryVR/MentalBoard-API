fce0266db6c27dbbb85d6bd21259793c95fc9443
LauryV <laury.vaquedano@yahoo.com>
2020-04-08 04:24:08 -0600

    Changing

---
+++ MentalBoard-API/Controllers/AppUsersController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MentalBoard_API.Contexts;
+using MentalBoard_API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace MentalBoard_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppUsersController : ControllerBase
+    {
+        
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IConfiguration config;
+
+        public AppUsersController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            this.config = configuration;
+        }
+
+        /// <summary>
+        /// Create a new user.
+        /// </summary>
+        [Route("Create")]
+        [HttpPost]
+        public async Task<IActionResult> CreateUser([FromBody] AppUser model)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                var result = await _userManager.CreateAsync(model, model.PasswordHash);
+                if (result.Succeeded)
+                {
+                    return BuildToken(model);
+                }
+                else
+                {
+                    return BadRequest("Invalid data.");
+                }
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+
+        }
+
+        /// <summary>
+        /// Login of a user.
+        /// </summary>
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] AppUser userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(userInfo.UserName, userInfo.PasswordHash, isPersistent: false, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return BuildToken(userInfo);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return BadRequest(ModelState);
+                }
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        private IActionResult BuildToken(AppUser userInfo)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.UniqueName, userInfo.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["CRYPT_KEY"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddHours(3);
+
+            JwtSecurityToken token = new JwtSecurityToken(
+               claims: claims,
+               expires: expiration,
+               signingCredentials: creds);
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = expiration
+            });
+
+        }
+
+
+    }
+}
--- MentalBoard-API/Startup.cs
+++ MentalBoard-API/Startup.cs
@@ -17,6 +17,9 @@ using System.Reflection;
 using System.IO;
 using MentalBoard_API.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace MentalBoard_API
 {
@@ -46,6 +49,19 @@ namespace MentalBoard_API
             services.AddIdentity<AppUser, AppRole>()
                 .AddEntityFrameworkStores<MentalBoardContext>()
                 .AddDefaultTokenProviders();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                 options.TokenValidationParameters = new TokenValidationParameters
+                 {
+                     ValidateIssuer = true,
+                     ValidateAudience = true,
+                     ValidateLifetime = true,
+                     ValidateIssuerSigningKey = true,
+                     IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(Configuration["CRYPT_KEY"])),
+                     ClockSkew = TimeSpan.Zero
+                 });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
